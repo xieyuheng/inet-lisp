2b5ca18926157c474edcfd7c2dd45d0c63750d10
Xie Yuheng <xyheme@gmail.com>
2025-04-25 02:00:34 +0800

    [net] `node_neighborhood_destroy`

--- src/net/node_neighborhood.c
+++ src/net/node_neighborhood.c
@@ -7,3 +7,14 @@ node_neighborhood_new(node_t *node) {
     self->node_neighbor_array = array_new_auto();
     return self;
 }
+
+void
+node_neighborhood_destroy(node_neighborhood_t **self_pointer) {
+    assert(self_pointer);
+    if (*self_pointer == NULL) return;
+
+    node_neighborhood_t *self = *self_pointer;
+    array_destroy(&self->node_neighbor_array);
+    free(self);
+    *self_pointer = NULL;
+}
--- src/net/node_neighborhood.h
+++ src/net/node_neighborhood.h
@@ -6,3 +6,4 @@ struct node_neighborhood_t {
 };
 
 node_neighborhood_t *node_neighborhood_new(node_t *node);
+void node_neighborhood_destroy(node_neighborhood_t **self_pointer);
