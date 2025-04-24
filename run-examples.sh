2a2d8030237152d2673467825d6b40ae3f079f48
Xie Yuheng <xyheme@gmail.com>
2025-04-25 00:20:58 +0800

    up

--- run-examples.sh
+++ run-examples.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-bin=./bin/inet-lisp
+bin="./bin/inet-lisp run"
 ext=lisp
 
 for example in $(find examples -name "*.${ext}" -not -name "*.test.${ext}" -not -name "*.error.${ext}" -not -name "*.play.${ext}"); do
     echo "[run] $example"
-    ${bin} run $example
+    ${bin} $example
 done
 
 for example in $(find examples -name "*.test.${ext}"); do
     echo "[test] $example"
-    ${bin} run $example > $example.out
+    ${bin} $example > $example.out
 done
 
 for example in $(find examples -name "*.error.${ext}"); do
     echo "[error] $example"
-    ${bin} run $example 2> $example.err || true
+    ${bin} $example 2> $example.err || true
 done
