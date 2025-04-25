a4f813e8244b354c0a674fd359eed2ebd12ad14b
Xie Yuheng <xyheme@gmail.com>
2025-04-26 02:34:29 +0800

    linear-variable.error.lisp

---
+++ examples/tests/linear-variable.error.lisp
@@ -0,0 +1,4 @@
+(define (int-square x)
+  (imul x x))
+
+(assert (eq? (int-square 2) 4))
--- src/core/frame.c
+++ src/core/frame.c
@@ -22,11 +22,11 @@ frame_destroy(frame_t **self_pointer) {
     assert(self_pointer);
     if (*self_pointer == NULL) return;
 
-     frame_t *self = *self_pointer;
-     // does not own function
-     array_destroy(&self->variable_array);
-     free(self);
-     *self_pointer = NULL;
+    frame_t *self = *self_pointer;
+    // does not own function
+    array_destroy(&self->variable_array);
+    free(self);
+    *self_pointer = NULL;
 }
 
 bool
@@ -52,12 +52,31 @@ frame_print(const frame_t *self, file_t *file) {
     fprintf(file, "</frame>\n");
 }
 
+
 value_t
 frame_get_variable(const frame_t *self, size_t index) {
-    return array_get(self->variable_array, index);
+    value_t value = array_get(self->variable_array, index);
+    if (!value) {
+        who_printf("undefined variable index: %lu\n", index);
+        exit(1);
+    }
+
+    // NOTE be linear like this:
+    array_set(self->variable_array, index, NULL);
+    return value;
 }
 
 void
 frame_set_variable(frame_t *self, size_t index, value_t value) {
+    value_t found = array_get(self->variable_array, index);
+    // NOTE be linear like this:
+    if (found) {
+        who_printf("variable index is already used: %lu\n", index);
+        who_printf("found value: ");
+        value_print(found, stderr);
+        fprintf(stderr, "\n");
+        exit(1);
+    }
+
     array_set(self->variable_array, index, value);
 }
