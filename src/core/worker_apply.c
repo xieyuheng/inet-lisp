19a80cade8bfa8cbf5dc25c1879245f235188528
Xie Yuheng <xyheme@gmail.com>
2025-04-27 22:04:45 +0800

    up

--- src/core/worker_apply.c
+++ src/core/worker_apply.c
@@ -13,13 +13,13 @@ worker_apply(worker_t *worker, value_t target, size_t arity) {
         stack_push(worker->return_stack, frame);
     } else if (is_primitive(target)) {
         primitive_t *primitive = as_primitive(target);
-        // TODO supplement wire if arity is short
-        assert(primitive->input_arity == arity);
-        if (!primitive->node_ctor) {
-            worker_apply_primitive(worker, primitive);
-        } else {
+        if (primitive->node_ctor) {
             node_t *node = worker_new_node(worker, primitive->node_ctor);
             worker_reconnect_node(worker, node, arity);
+        } else {
+            // TODO supplement wire if arity is short
+            assert(primitive->input_arity == arity);
+            worker_apply_primitive(worker, primitive);
         }
     } else if (is_wire(target)) {
         value_t defuzed = defuze(target);
