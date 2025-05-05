eb718659fe3bb24e0ad1a0655f7a1714e9477429
Xie Yuheng <xyheme@gmail.com>
2025-05-05 22:46:43 +0800

    `cmd_info` -- print cache size for `__GLIBC__`

--- src/commands/cmd_info.c
+++ src/commands/cmd_info.c
@@ -15,6 +15,18 @@ run(commander_t *commander) {
     (void) commander;
 
     printf("page size: %ld bytes\n", sysconf(_SC_PAGE_SIZE));
+
+#if __GLIBC__
+    printf("l1 cache size: %ld bytes\n", sysconf(_SC_LEVEL1_DCACHE_SIZE));
+    printf("l1 cache line size: %ld bytes\n", sysconf(_SC_LEVEL1_DCACHE_LINESIZE));
+
+    printf("l2 cache size: %ld bytes\n", sysconf(_SC_LEVEL2_CACHE_SIZE));
+    printf("l2 cache line size: %ld bytes\n", sysconf(_SC_LEVEL2_CACHE_LINESIZE));
+
+    printf("l3 cache size: %ld bytes\n", sysconf(_SC_LEVEL3_CACHE_SIZE));
+    printf("l3 cache line size: %ld bytes\n", sysconf(_SC_LEVEL3_CACHE_LINESIZE));
+#endif
+
     printf("number of processors: %ld\n", sysconf(_SC_NPROCESSORS_ONLN));
     printf("size of time_t: %ld bits\n", sizeof(time_t) * 8);
 
