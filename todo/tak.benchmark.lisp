e2775808fb5f364db7145529c4760b56a37b87e6
Xie Yuheng <xyheme@gmail.com>
2025-05-14 19:21:17 +0800

    up

--- todo/tak.benchmark.lisp
+++ todo/tak.benchmark.lisp
@@ -1,20 +1,35 @@
-(define-node tak x! y z result)
-(define-node tak-int x y! z result)
+;; suppose we have multiple principal ports.
+
+(define-node tak x! y! z result)
 (define-node tak-if p! x y z result)
 
-(define-rule (tak (int? x) y z result)
-  (tak-int x y z result))
+;; suppose we have typed rule, and auto dup/drop.
 
-(define-rule (tak-int x (int? y) z result)
+(define-record int-i
+  :is int?
+  :dup int-dup
+  :drop int-drop)
+
+(define-rule (tak (is x int-i) (is y int-i) z result)
   (tak-if (int-lt? y x) x y z result))
 
-(define-rule (tak-if (bool? p) x y z result)
-  (if p
+;; suppose we have builtin const `true` and `false`.
+
+(define-rule (tak-if true x y z result)
+  (tak (tak (isub x 1) y z)
+       (tak (isub y 1) z x)
+       (tak (isub z 1) x y)
+       result))
+
+(define-rule (tak-if false x y z result)
+  (connect result z))
+
+;; inline `tak-if`:
+
+(define-rule (tak (is x int-i) (is y int-i) z result)
+  (if (int-lt? y x)
     (tak (tak (isub x 1) y z)
          (tak (isub y 1) z x)
          (tak (isub z 1) x y)
          result)
-    (begin
-      (int-drop x)
-      (int-drop y)
-      (connect z result))))
+    (connect result z)))
